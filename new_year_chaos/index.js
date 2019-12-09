bb2b724aab2ad48f0f205c13c238f1690c4a6130
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-12-09 11:16:42 +0100

    Polya for NYC

--- new_year_chaos/index.js
+++ new_year_chaos/index.js
@@ -7,16 +7,35 @@
 // Return the minimum number of bribes needed to create a given queue of people
 
 function minimumBribes(q) {
+    // Loop over q
+    for (let i = 0; i < q.length; i++) {
+        console.log(q[i])
 
+        console.log(i)
+        if (q[i] - [i+1] > 2) {
+            // return "Too chaotic"
+            console.log("Too chaotic")
+        }
+
+        // Find the difference between the value at i and where it's supposed to be
+            // if diff == 1
+                // add one to min_bribes
+            // if diff == 2
+                // add two to mind_bribes
+    }
+        // If the value of an item in q - i > 2
+            // return "Too chaotic"
+    console.log("done")
+        
 
 }
 console.log(minimumBribes([2, 1, 5, 3, 4]))
 // 3
 // console.log(minimumBribes([2, 5, 1, 3, 4]))
-// // Too chaotic
+// Too chaotic
 // console.log(minimumBribes([5, 1, 2, 3, 7, 8, 6, 4]))
-// // Too chaotic
+// Too chaotic
 // console.log(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]))
-// // 7
+// 7
 // console.log(minimumBribes([1, 2, 5, 3, 4, 7, 8, 6]))
 // // 4
