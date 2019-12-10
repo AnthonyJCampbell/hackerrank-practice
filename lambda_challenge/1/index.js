3d651c2dace78deb4ac6801a4548de3aab78ff5c
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-12-10 08:44:54 +0100

    Exercise 1 of Lambda challenge

---
+++ lambda_challenge/1/index.js
@@ -0,0 +1,21 @@
+function fibonacci(n) {
+    // Simplest way to do it must be iteratively
+    // Declare current and next
+
+    const return_arr = []
+    let current = 0
+    let next = 1
+
+    // Iterate over n
+    for (let i = 0; i < n; i++) {
+        // For every step, add current to return_arr
+        return_arr.push(current)
+        // Store next
+        const holder = current
+        // Current becomes next
+        current = next
+        // Next becomes current + next
+        next = current + holder
+    }
+    return return_arr
+}
