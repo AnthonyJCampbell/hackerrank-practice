f90bc910c1472b35710f9f1ab0cff42ae6680a0f
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-12-12 15:08:52 +0100

    Cleaning up

--- min_abs_diff/index.js
+++ min_abs_diff/index.js
@@ -9,29 +9,17 @@
 // 
 
 function minimumAbsoluteDifference(arr) {
-    // With the given array, we'll need to find all possible pairs
-    let pairs = []
     let smallest_diff = Math.abs(arr[0] - arr[1])
-    // console.log(smallest_di)
 
     // Loop over every item in the array while i < arr.length - 1
     for (let i = 0; i < arr.length - 1; i++) {
         // Loop over the rest of the array
         for (let j = i + 1; j < arr.length; j++) {
-            pairs.push([arr[i], arr[j]])
             let distance = Math.abs(arr[i] - arr[j])
             if (Math.abs(distance) < smallest_diff) smallest_diff = distance
         }
     }
-    // // Initialize to first value so that we always get a correct value
-    // let smallest_diff = Math.abs(pairs[0][0] - pairs[0][1])
 
-    // // For each pair, evaluate the distance between the two (x - y)
-    // for (const pair in pairs) {
-    //     let distance = Math.abs(pairs[pair][0] - pairs[pair][1])
-    //     if (Math.abs(distance) < smallest_diff) smallest_diff = distance
-    // }
-    
     return smallest_diff
     
 }
