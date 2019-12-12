8570d4092af028c3f05e9687b2ea45cb6f1a50c0
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-12-12 14:31:49 +0100

    Starting min_abs_diff

---
+++ min_abs_diff/index.js
@@ -0,0 +1,21 @@
+// Given an array of integers, find the smallest difference possible between two of the integers in the array, as seen when drawing a direct line between the two
+
+// Return that integer
+
+// Example: given [2, -2, 4], the results would be:
+//      2 - (-2) = 4
+//      4 - 2 = 2
+//      (-2) - 4 = 6
+// 
+
+function minimumAbsoluteDifference(arr) {
+    // With the given array, we'll need to find all possible pairs
+
+    // For each pair, evaluate the distance between the two (x - y)
+        // if distance < smallest_diff
+            // smallest_dif = distance
+
+    // return smallest_diff
+
+}
+console.log(minimumAbsoluteDifference([3, -7, 0]))
