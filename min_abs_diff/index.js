1866d2a20446fb3200c73ef3242722f7c73f17ae
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-12-12 15:05:57 +0100

    Most tests pass. Looking for more efficient approach

--- min_abs_diff/index.js
+++ min_abs_diff/index.js
@@ -10,12 +10,35 @@
 
 function minimumAbsoluteDifference(arr) {
     // With the given array, we'll need to find all possible pairs
+    let pairs = []
+    let smallest_diff = Math.abs(arr[0] - arr[1])
+    // console.log(smallest_di)
 
-    // For each pair, evaluate the distance between the two (x - y)
-        // if distance < smallest_diff
-            // smallest_dif = distance
+    // Loop over every item in the array while i < arr.length - 1
+    for (let i = 0; i < arr.length - 1; i++) {
+        // Loop over the rest of the array
+        for (let j = i + 1; j < arr.length; j++) {
+            pairs.push([arr[i], arr[j]])
+            let distance = Math.abs(arr[i] - arr[j])
+            if (Math.abs(distance) < smallest_diff) smallest_diff = distance
+        }
+    }
+    // // Initialize to first value so that we always get a correct value
+    // let smallest_diff = Math.abs(pairs[0][0] - pairs[0][1])
 
-    // return smallest_diff
-
+    // // For each pair, evaluate the distance between the two (x - y)
+    // for (const pair in pairs) {
+    //     let distance = Math.abs(pairs[pair][0] - pairs[pair][1])
+    //     if (Math.abs(distance) < smallest_diff) smallest_diff = distance
+    // }
+    
+    return smallest_diff
+    
 }
-console.log(minimumAbsoluteDifference([3, -7, 0]))
+console.log(minimumAbsoluteDifference([-59, -36, -13, 1, -53, -92, -2, -96, -54, 75]))
+// console.log(Math.abs(-54 - -53))
+// -53
+// -54
+// console.log(pairs[33])
+// console.log(Math.abs(pairs[33][0] % Infinity) + Math.abs(pairs[33][1] % Infinity))
+// console.log(pairs[26][0] - pairs[26][1])
